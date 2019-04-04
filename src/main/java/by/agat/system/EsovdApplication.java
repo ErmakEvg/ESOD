3d173bca81db941b71dd12c1dc6132b2cb17c340
Женька <iermakovich.i@mail.ru>
2019-04-04 16:16:37 +0300

    Initial commit

---
+++ src/main/java/by/agat/system/EsovdApplication.java
@@ -0,0 +1,13 @@
+package by.agat.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsovdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsovdApplication.class, args);
+    }
+
+}
